b4f58c9ae091f44263c11d49162e9e079abf0764
FLEXISOURCEIT\gcyabut <gian.yabut@yahoo.com>
2017-02-23 07:55:55 +0800

    Initial commit

---
+++ Simple-SignalR-Core/src/Simple-SignalR-Core/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simple_SignalR_Core
+{
+    public class ChatHub : Hub
+    {
+        public static List<string> ConnectedUsers;
+
+        public void Send(string originatorUser, string message)
+        {
+            Clients.All.messageReceived(originatorUser, message);
+        }
+
+        public void Connect(string newUser)
+        {
+            if (ConnectedUsers == null)
+                ConnectedUsers = new List<string>();
+
+            ConnectedUsers.Add(newUser);
+            Clients.Caller.getConnectedUsers(ConnectedUsers);
+            Clients.Others.newUserAdded(newUser);
+        }
+    }
+}
